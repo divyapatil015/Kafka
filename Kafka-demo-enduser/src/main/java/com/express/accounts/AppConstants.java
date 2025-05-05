2abac4e6638a0fc0fba48f6b20e547d4f3445d58
Divya Patil <divyapatil336@gmail.com>
2025-05-05 18:41:05 +0530

    first commit

---
+++ Kafka-demo-enduser/src/main/java/com/express/accounts/AppConstants.java
@@ -0,0 +1,11 @@
+package com.express.accounts;
+
+public class AppConstants {
+
+	public static final String LOCATION_UPDATE_TOPIC="location-update-topic";
+	
+	public static final  String GROUP_ID="grouop-1";
+	
+}
+
+
